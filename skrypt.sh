6d75b055ce59dc6ff5346be04851e164a2a07034
ddssww51 <pasek.mikolaj26@outlook.com>
2023-04-13 11:49:31 +0200

    test3

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,12 @@
 
 if [[ $1 == "--date" ]]; then
   echo "Aktualna data i godzina: $(date)"
+elif [[ $1 == "--logs" ]]; then
+  for i in {1..100}
+  do
+    filename="log${i}.txt"
+    echo -e "Nazwa pliku: ${filename}\nUtworzony przez: ${0}\nData: $(date)" > "${filename}"
+  done
 else
-  echo "Nie prawidłowa flaga. Prawidłowa flaga --date"
+  echo "Nie prawidłowe flagi. Prawidłowe flagi: --date, --logs"
 fi
